ac030c0330081a6d717a4bd70db0c09725d7af67
DESKTOP-S7J6ADN\Refnaldy <itpolsri@gmail.com>
2018-04-10 22:42:18 +0700

    Update index and assets

---
+++ assets/js/refnaldy.js
@@ -0,0 +1,3 @@
+var modal = document.getElementById('ticketModal'); window.onclick = function (event) { if (event.target == modal) { modal.style.display = "none" } }
+function toggleFunction() { var x = document.getElementById("navDemo"); if (x.className.indexOf("ref-show") == -1) { x.className += " ref-show" } else { x.className = x.className.replace(" ref-show", "") } }
+window.onscroll = function () { myFunction() }; function myFunction() { var navbar = document.getElementById("myNavbar"); if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) { navbar.className = "ref-bar" + " ref-card" + " ref-animate-top" + "ref-white" } else { navbar.className = navbar.className.replace(" ref-card ref-animate-top ref-tale", "") } }
